f0c1494101bf3d57c1ead3022c4e345d90e9b32c
Sally Lee-Duffy <sally@thesnowinstitute.com>
2023-12-29 17:49:54 +0100

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Sally Lee-Duffy and is{" "}
-        <a
-          href="https://github.com/sallyelaineleeduffy/react-weather.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Sally Lee-Duffy and is{" "}
+          <a
+            href="https://github.com/sallyelaineleeduffy/react-weather.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
