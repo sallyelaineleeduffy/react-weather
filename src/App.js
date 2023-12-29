b06fc4403ac44807eb651662be89e500a811e8ca
Sally Lee-Duffy <sally@thesnowinstitute.com>
2023-12-29 16:44:07 +0100

    Added Github repo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Sally Lee-Duffy and is{" "}
+        <a
+          href="https://github.com/sallyelaineleeduffy/react-weather.git"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
