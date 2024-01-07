735660fe11338d11b5934b4b5507771817d2be74
Sally Lee-Duffy <sally@thesnowinstitute.com>
2024-01-07 23:05:33 +0100

    Update CSS

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,7 +17,7 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-start">
-              <WeatherIcon code={props.data.icon} size={52} />
+              <WeatherIcon code={props.data.icon} size={60} />
             </div>
 
             <div className="float-center">
